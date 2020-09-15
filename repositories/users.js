1a616cf7b1516a4b1cd825abe97e4104ee76308f
Andy <mcgow_a@hotmail.com>
2020-09-15 16:47:20 +0000

    interim commit

--- repositories/users.js
+++ repositories/users.js
@@ -45,19 +45,35 @@ class UsersRepository {
         const records = await this.getAll();
         return records.find(record => record.id === id);
     }
+	
+	async delete(id) {
+		const records = await this.getAll();
+		const filteredRecords = records.filter(record => record.id !== id);
+		await this.writeAll(filteredRecords);
+	}
+	
+	async update(id, attrs) {
+		const records = await this.getAll();
+		const record = records.find(record => record.id === id);
+		
+		if (!record) {
+			throw new Error(`Record with id ${id} not found`);
+		}
+		
+		Object.assign(record, attrs);
+		await this.writeAll(records);
+	}
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
-
-    await repo.create({ email: 'test@test.com', password: 'password' });
-
-    const users = await repo.getAll();
-    console.log(users);
-
-    const user = await repo.getOne('agagagag');
-    console.log(user);
-
+	
+	// await repo.create({ email: 'test@test.com', password: 'password'});
+	
+	await repo.update('agaga', { password: "smellslikewhisky"});
+	
+	console.log(await repo.getAll())
+	
 }
 
 test();
