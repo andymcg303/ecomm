91094a98d4aa0929a2a57e7ec451d023d88f1840
andymcg303 <mcgow_a@hotmail.com>
2020-09-26 20:36:06 +0100

    interim commit

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,16 +1,17 @@
 const layout = require('../../layout');
 
 module.exports = ({ req }) => {
-    return layout({ content: `
-        <div>
-            Your id is ${req.session.userId} 
-            <form method="POST">
-                <input name="email" placeholder="email">
-                <input name="password" placeholder="password">
-                <input name="passwordConfirmation" placeholder="confirm password">
-                <button>Sign Up</button>
-            </form>
-        </div>
+    return layout({ 
+        content: `
+            <div>
+                Your id is ${req.session.userId} 
+                <form method="POST">
+                    <input name="email" placeholder="email">
+                    <input name="password" placeholder="password">
+                    <input name="passwordConfirmation" placeholder="confirm password">
+                    <button>Sign Up</button>
+                </form>
+            </div>
         ` 
     });
 }
