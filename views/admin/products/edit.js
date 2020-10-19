0515123fb00c4197b9c3d75b1cf237ad4a9cf698
andymcg303 <mcgow_a@hotmail.com>
2020-10-19 11:58:18 +0100

    interim commit

--- views/admin/products/edit.js
+++ views/admin/products/edit.js
@@ -5,7 +5,7 @@ module.exports = ({ product }) => {
     content: `
         <form method="POST">
             <input name="title" value="${product.title}">
-            <input name="title" value="${product.price}">
+            <input name="price" value="${product.price}">
             <input name="image" type="file">
             <button>Submit</button>
         </form>
