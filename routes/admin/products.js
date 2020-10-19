b23e3195140e2251fc32cbe8dd9e8f303e3d4f13
andymcg303 <mcgow_a@hotmail.com>
2020-10-19 09:21:38 +0100

    to fix but in updating product

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -40,14 +40,36 @@ router.post('/admin/products/new',
 );
 
 router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
-    const product = await productsRepo.getOneBy(req.params.id);
+    const product = await productsRepo.getOne(req.params.id);
     
     if (!product) {
-        return res.send('Product not fount');
+        return res.send('Product not found');
     }
 
     res.send(productsEditTemplate({ product }));
 
-})
+});
+
+router.post('/admin/products/:id/edit', 
+    requireAuth, 
+    upload.single('image'),
+    [requireTitle, requirePrice],
+    handleErrors(productsEditTemplate), //wont work
+    async (req, res) => {
+        const changes = req.body;
+
+        if (req.file) {
+            changes.image = req.file.buffer.toString('base64');
+        }
+
+        try {
+            await productsRepo.update(req.params.id, changes);
+        } catch (err) {
+            return res.send('Could not find item to update');
+        
+        }
+        res.redirect('/admin/products');
+    }
+);    
 
 module.exports = router;
