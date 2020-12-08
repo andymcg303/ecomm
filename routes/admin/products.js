9e49a121bf21baaa14d6299164e196bc585251c8
Andy McG <mcgow_a@hotmail.com>
2020-12-08 18:44:35 +0000

    interim commit

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -75,4 +75,9 @@ router.post('/admin/products/:id/edit',
     }
 );    
 
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+    await productsRepo.delete(req.params.id);
+    res.redirect('/admin/products');
+})
+
 module.exports = router;
